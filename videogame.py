3b3046b5103688408be5613e242ee46e261c11a6
LoneKakarrot <63334227+LoneKakarrot@users.noreply.github.com>
2022-07-31 12:21:13 -0400

    Add files via upload

--- videogame.py
+++ videogame.py
@@ -1,11 +1,31 @@
 import turtle
 
+
 def forw():
     turtle.forward(10) # Values subject to change
-    
+    print(turtle.xcor())
+    print(turtle.ycor())
+    if turtle.xcor() > 330.0:
+        turtle.reset()
+    elif turtle.xcor() < -330.0:
+        turtle.reset()
+    elif turtle.ycor() > 270.0:
+        turtle.reset()
+    elif turtle.ycor() < -270.0:
+        turtle.reset()
+        
 def back():
     turtle.backward(10) # Values sunject to change
-    
+    print(turtle.xcor())
+    print(turtle.ycor())
+    if turtle.xcor() > 330.0:
+        turtle.reset()
+    elif turtle.xcor() < -330.0:
+        turtle.reset()
+    elif turtle.ycor() > 270.0:
+        turtle.reset()
+    elif turtle.ycor() < -270.0:
+        turtle.reset()
 def left():
     turtle.left(90)    # Values sunject to change
 
@@ -17,3 +37,5 @@ turtle.onkeypress(back, "Down")
 turtle.onkeypress(left, "Left")
 turtle.onkeypress(right, "Right")
 turtle.listen()
+
+turtle.mainloop()
