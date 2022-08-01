5ea6115c352cea8b894fb3d01db7e3540c7a37da
LoneKakarrot <63334227+LoneKakarrot@users.noreply.github.com>
2022-08-01 12:58:17 -0400

    Add files via upload

--- videogame.py
+++ videogame.py
@@ -1,5 +1,5 @@
 import turtle
-
+turtle.textinput('Lone Freeman', 'Welcome to the Lone Freeman! Press enter to start.')
 
 def forw():
     turtle.forward(10) # Values subject to change
@@ -13,9 +13,10 @@ def forw():
         turtle.reset()
     elif turtle.ycor() < -270.0:
         turtle.reset()
-        
+    obst(turtle,xpositions, ypositions)
+    collision(xpositions, ypositions)
 def back():
-    turtle.backward(10) # Values sunject to change
+    turtle.backward(10) # Values subject to change
     print(turtle.xcor())
     print(turtle.ycor())
     if turtle.xcor() > 330.0:
@@ -26,16 +27,68 @@ def back():
         turtle.reset()
     elif turtle.ycor() < -270.0:
         turtle.reset()
+    obst(turtle,xpositions, ypositions)
+    collision(xpositions, ypositions)
 def left():
-    turtle.left(90)    # Values sunject to change
+    turtle.left(90)    # Values subject to change
 
 def right():
-    turtle.right(90)   # Values sunject to change
+    turtle.right(90)   # Values subject to change
 
+
+    
 turtle.onkeypress(forw, "Up")
 turtle.onkeypress(back, "Down")
 turtle.onkeypress(left, "Left")
 turtle.onkeypress(right, "Right")
+
 turtle.listen()
 
+turtle.bgcolor("red")
+
+
+
+
+xpositions = []
+ypositions = []    
+    
+def obst(turtle,xpositions, ypositions):
+    
+    dist = 200
+    for i in range(5):
+        
+        tur1 = turtle.Turtle()
+        
+        tur1.shape('triangle')
+        
+        tur1.penup()
+        tur1.goto(0,0)
+        tur1.forward(dist)
+        tur1.left(90)
+        tur1.pendown()
+        xpositions.append(tur1.xcor())
+        ypositions.append(tur1.ycor())
+        print(xpositions)
+        print(ypositions)
+        dist = dist + 100
+    return xpositions, ypositions
+
+def collision(xpositions, ypositions):
+    x = 0
+    for i in xpositions:
+        print(int(xpositions[x]) - int(turtle.xcor()))
+        if int(xpositions[x]) - int(turtle.xcor()) <= abs(1) and int(ypositions[x]) - int(turtle.ycor()) <= abs(1):
+            turtle.reset()
+            turtle.clearscreen()
+
+            
+        x = x + 1
+    
+    
+    
+    
+    
+
+
 turtle.mainloop()
+
